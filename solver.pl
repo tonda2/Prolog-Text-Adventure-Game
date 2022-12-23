3e82946e653f4e9c13f8d6372d21925e643da3f0
Jan Antonin Musil <musilj30@fit.cvut.cz>
2022-12-23 21:49:03 +0100

    Check grid size

---
+++ solver.pl
@@ -0,0 +1,22 @@
+:- use_module(library(lists)).
+
+equal(X,X).
+	
+allSameLength([Row1, Row2]) :- 
+	length(Row1, First),
+	length(Row2, Second),
+	equal(First, Second).
+	
+allSameLength([Row1, Row2|Rest]) :-
+	length(Row1, First),
+	length(Row2, Second),
+	equal(First, Second),
+	allSameLength([Row2|Rest]).
+
+properSize(Sudoku, Goal) :- 
+	length(Sudoku, Goal),
+	nth0(0, Sudoku, FirstRow),
+	length(FirstRow, Goal),
+	allSameLength(Sudoku).
+
+solve(Sudoku, Size) :- properSize(Sudoku, Size).
